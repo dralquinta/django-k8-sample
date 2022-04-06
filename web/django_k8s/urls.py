d9d2f241ab6e410f6509e3719e97219c85823384
GrzegorzWysopal <wysopalgrzegorz@gmail.com>
2022-04-06 10:02:04 +0000

    Upload app created. Changes made in settings.py so the app can work properly https://github.com/dralquinta/django-k8-sample/issues/11

--- web/django_k8s/urls.py
+++ web/django_k8s/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('upload/', include('upload.urls')),
 ]
+
+# This part is related to upload app
+# It helps to locate uploaded media files
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ web/upload/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+
+def index(request):
+    context = {}
+    if request.method == 'POST':
+        uploaded_file = request.FILES['document']
+        fs = FileSystemStorage()
+        name = fs.save(uploaded_file.name, uploaded_file)
+        context['url'] = fs.url(name)
+    return render(request, 'upload.html',context)
+    
+
